27e96184e165de45b891414ac6a4b53ce1fd9835
Nicholas Goodwin <nick@Nicholass-MacBook-Pro.local>
2024-03-08 20:23:49 +0000

    completed ch01 exercises

--- chapter1/which.go
+++ chapter1/which.go
@@ -12,24 +12,28 @@ func main() {
 		fmt.Println("Please provide an argument!")
 		return
 	}
-	file := arguments[1]
-	path := os.Getenv("PATH")
-	pathSplit := filepath.SplitList(path)
 
-	for _, directory := range pathSplit {
-		fullPath := filepath.Join(directory, file)
-		// Does it exist?
-		fileInfo, err := os.Stat(fullPath)
-		if err == nil {
-			mode := fileInfo.Mode()
-			// Is it a regular file?
-			if mode.IsRegular() {
-				// Is it executable?
-				if mode&0111 != 0 {
-					fmt.Println(fullPath)
-					return
+	for _, file := range arguments[1:] {
+		path := os.Getenv("PATH")
+		pathSplit := filepath.SplitList(path)
+
+		for _, directory := range pathSplit {
+			fullPath := filepath.Join(directory, file)
+			// Does it exist?
+			fileInfo, err := os.Stat(fullPath)
+			if err == nil {
+				mode := fileInfo.Mode()
+				// Is it a regular file?
+				if mode.IsRegular() {
+					// Is it executable?
+					if mode&0111 != 0 {
+						fmt.Println(fullPath)
+					}
 				}
 			}
 		}
+
+		fmt.Println()
 	}
+
 }
